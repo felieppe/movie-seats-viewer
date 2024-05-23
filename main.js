a1826ce2649d7c66dd0d5f7cd0cd6856ba9f10ff
Felipe Cabrera <me@felieppe.com>
2024-05-23 13:59:37 -0300

    Implemented API server

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const { app, BrowserWindow } = require('electron')
+const express = require('express')
 
+var win = null;
 function createWindow() {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -14,3 +16,26 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow)
+
+/*
+
+    API SERVER
+
+*/
+
+const api = express()
+const port = 3000;
+
+api.use(express.json())
+
+api.get('/seat/:id', (req, res) => {
+    let id = req.params.id;
+    if (id == null || id == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing ID of seat"}}); return; }
+
+    win.webContents.send('book-seat', id)
+    res.send(`Seat ${id} is booked!`)
+})
+
+api.listen(port, () => {
+    console.log(`API server listening at http://localhost:${port}`)
+})
