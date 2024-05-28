4359fc3722e142c20017a8cf15bc9ea14bfd3808
Felipe Cabrera <me@felieppe.com>
2024-05-28 08:50:20 -0300

    Added route for get status of id related seat

--- api/routes/seats/index.js
+++ api/routes/seats/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const { ipcMain } = require('electron')
 
 router.get('/:id', (req, res) => {
     // Get status of the seat related to the ID provided.
+
+    let id = req.params.id;
+    if (id == null || id == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing ID of seat"}}); return; }
+
+    let win = req.win
+    win.webContents.send('get-seat-status', id)
+
+    ipcMain.once('data-output', (event, data) => {
+        if (data["type"] == "seat-status-output") {
+            res.status(200).json({success: true, data: data["data"]})
+        }
+    })
 })
 
 router.post('/:id', (req, res) => {
