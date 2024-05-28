16aa20bff1a5264b617d1c574d553e7e79c8e791
Felipe Cabrera <me@felieppe.com>
2024-05-28 09:00:22 -0300

    Added workflow and routing of release-seat process

--- api/routes/seats/index.js
+++ api/routes/seats/index.js
@@ -38,6 +38,20 @@ router.post('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     // Try to release the seat related to the ID provided.
+
+    let id = req.params.id;
+    if (id == null || id == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing ID of seat"}}); return; }
+
+    let win = req.win
+    win.webContents.send('release-seat', id)
+
+    ipcMain.once('data-output', (event, data) => {
+        if (data["type"] == "seat-released-output") {
+            if (data["data"]["released"]) {
+                res.status(200).json({success: true, data: data["data"]})
+            } else { res.status(400).json({success: false, error: { code: 400, message: data["data"]["error"]}}) }
+        }
+    })
 })
 
 module.exports = router
