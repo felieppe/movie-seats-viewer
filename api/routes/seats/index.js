083d0af32fd7cd582507667624a17ba24b50c0eb
Felipe Cabrera <me@felieppe.com>
2024-05-28 08:57:00 -0300

    Improved routing and workflow of book_seat process

--- api/routes/seats/index.js
+++ api/routes/seats/index.js
@@ -27,7 +27,13 @@ router.post('/:id', (req, res) => {
     let win = req.win
     win.webContents.send('book-seat', id)
 
-    res.send(`Seat ${id} is booked!`)
+    ipcMain.once('data-output', (event, data) => {
+        if (data["type"] == "seat-booked-output") {
+            if (data["data"]["booked"]) {
+                res.status(200).json({success: true, data: data["data"]})
+            } else { res.status(400).json({success: false, error: { code: 400, message: data["data"]["error"]}}) }
+        }
+    })
 })
 
 router.delete('/:id', (req, res) => {
