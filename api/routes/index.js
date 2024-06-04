0c57dc0f849b6c18006a2cbd88b5863d6af805bb
Felipe Cabrera <me@felieppe.com>
2024-06-04 09:22:46 -0300

    Added /heartbeat/ routing to API

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const seats = require('./seats')
+const heartbeat = require('./heartbeat')
 
 router.use('/seats/', seats)
+router.use('/heartbeat/', heartbeat)
 
 module.exports = router
