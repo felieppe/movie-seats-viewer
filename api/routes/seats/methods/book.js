95e5b4c5e4ffb83c566754f084a3cd0712e62382
Felipe Cabrera <me@felieppe.com>
2024-06-06 10:11:39 -0300

    feat(app): add client id handling from reqs so a set is limited to one client

--- api/routes/seats/methods/book.js
+++ api/routes/seats/methods/book.js
@@ -3,11 +3,14 @@ const router = express.Router()
 const { ipcMain } = require('electron')
 
 router.post('/:id', (req, res) => {
-    let id = req.params.id;
+    let { id } = req.params
+    let { client } = req.body
+
     if (id == null || id == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing ID of seat"}}); return; }
+    if (client == null || client == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing client information"}}); return; }
 
     let win = req.win
-    win.webContents.send('book-seat', id)
+    win.webContents.send('book-seat', id, client)
 
     ipcMain.once('data-output', (event, data) => {
         if (data["type"] == "seat-booked-output") {
--- api/routes/seats/methods/reserve.js
+++ api/routes/seats/methods/reserve.js
@@ -5,11 +5,14 @@ const { ipcMain } = require('electron')
 router.post('/:id', (req, res) => {
     // Try to reserve the seat related to the ID provided.
 
-    let id = req.params.id;
+    let { id } = req.params;
+    let { client } = req.body
+
     if (id == null || id == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing ID of seat"}}); return; }
+    if (client == null || client == "") { res.status(400).json({success: false, error: { code: 400, message: "Missing client information"}}); return; }
 
     let win = req.win
-    win.webContents.send('reserve-seat', id)
+    win.webContents.send('reserve-seat', id, client)
 
     ipcMain.once('data-output', (event, data) => {
         if (data["type"] == "seat-reserved-output") {
